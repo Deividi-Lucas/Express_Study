6929ab4d908da39ae066861ae478faf23c57f1d9
Deividi-Lucas <deividibaby6@gmail.com>
2023-05-03 20:29:25 -0400

    Feat: Learn method post in insomnia with express

--- index.js
+++ index.js
@@ -7,14 +7,36 @@ require('dotenv').config()
 let User = process.env.User
 let Password = process.env.Password
 
-mongoose.connect(
-  `mongodb+srv://${User}:${Password}@cluster0.6xvbxxi.mongodb.net/test`
-)
+app.use(express.json())
+
+mongoose
+  .connect(
+    `mongodb+srv://${User}:${Password}@cluster0.6xvbxxi.mongodb.net/test`
+  )
+  .then(() => {
+    console.log('Banco de dados conectado')
+  })
+  .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
   res.send('Server test')
 })
 
+app.post('/post', (req, res) => {
+  const title = req.body
+
+  // Os dados são inseridos no json da seguinte forma
+  // [
+  //   {
+  //     'title': 'dados'
+  //   }
+  // ]
+
+  let response = title
+
+  res.send(response)
+})
+
 app.listen(port, () => {
-  console.log(`Seu servidor está funcionando na porta ${port}`)
+  console.log(`Seu servidor está funcionando na porta ${port} `)
 })
